a56947aaac00bbbf62b7434a8c18df69abda3637
KotoriK <52659125+KotoriK@users.noreply.github.com>
2021-04-27 12:47:33 +0800

    init

---
+++ WindowsApplication2/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 3
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.Controls.Add(Me.Button9, 2, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button8, 1, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button7, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button6, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button5, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button4, 0, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button3, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Button1, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(9, 30)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(282, 259)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(82, 72)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(96, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(82, 72)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(189, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(82, 72)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(3, 89)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(82, 72)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(96, 89)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(82, 72)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(189, 89)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(82, 72)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(3, 175)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(82, 72)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(96, 175)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(82, 72)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(189, 175)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(82, 72)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(105, 303)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(86, 47)
+        Me.Button10.TabIndex = 1
+        Me.Button10.Text = "Start"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(310, 362)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+
+End Class
---
+++ WindowsApplication2/Form1.vb
@@ -0,0 +1,216 @@
+﻿Public Class Form1
+    Private nowStep As Steps
+    Private nowPan(9) As Steps
+    Public Enum Steps
+        Maru = 0
+        Ex = 1
+        N = 255
+    End Enum
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        clearP()
+        nowStep = 1
+    End Sub
+
+    Private Sub clearP()
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+
+        Button1.Enabled = True
+        Button2.Enabled = True
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Button5.Enabled = True
+        Button6.Enabled = True
+        Button7.Enabled = True
+        Button8.Enabled = True
+        Button9.Enabled = True
+        For i = 1 To 9
+            nowPan(i) = 255
+        Next
+    End Sub
+    Private Function getnextStepinString() As String
+        Select Case nowStep
+            Case 0
+                nowStep = 1
+                Return "X"
+            Case 1
+                nowStep = 0
+                Return "O"
+        End Select
+    End Function
+    Private Sub Check()
+        
+        '行
+        Dim temp
+        For start = 1 To 7 Step 3
+            temp = nowPan(start) + nowPan(start + 1) + nowPan(start + 2)
+            Select Case temp
+                Case 0
+                    'Maru Wins
+                    Wins(0)
+                    Exit Sub
+                Case 3
+                    'Ex Wins
+                    Wins(1)
+                    Exit Sub
+                Case Else
+            End Select
+        Next
+        '列
+        For start = 1 To 3 Step 1
+            temp = nowPan(start) + nowPan(start + 3) + nowPan(start + 6)
+            Select Case temp
+                Case 0
+                    'Maru Wins
+                    Wins(0)
+                    Exit Sub
+                Case 3
+                    'Ex Wins
+                    Wins(1)
+                    Exit Sub
+                Case Else
+            End Select
+        Next
+        temp = nowPan(1) + nowPan(5) + nowPan(9)
+        Select Case temp
+            Case 0
+                'Maru Wins
+                Wins(0)
+                Exit Sub
+            Case 3
+                'Ex Wins
+                Wins(1)
+                Exit Sub
+            Case Else
+        End Select
+        temp = nowPan(3) + nowPan(5) + nowPan(7)
+        Select Case temp
+            Case 0
+                'Maru Wins
+                Wins(0)
+                Exit Sub
+            Case 3
+                'Ex Wins
+                Wins(1)
+                Exit Sub
+            Case Else
+        End Select
+        Dim empty As Boolean
+        empty = False
+        For i = 1 To 9
+            If nowPan(i) = 255 Then
+                empty = True
+            End If
+        Next
+        If empty = False Then
+            MsgBox("Draw!")
+        End If
+    End Sub
+    Private Sub Wins(winner As Steps)
+        Select Case winner
+            Case 0
+                MsgBox("Congratulation!Maru wins")
+            Case 1
+                MsgBox("Congratulation!Cross wins")
+        End Select
+    End Sub
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button9.Text = p
+        Button9.Enabled = False
+        nowPan(9) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub TableLayoutPanel1_Paint(sender As Object, e As PaintEventArgs) Handles TableLayoutPanel1.Paint
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button8.Text = p
+        Button8.Enabled = False
+        nowPan(8) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button7.Text = p
+        Button7.Enabled = False
+        nowPan(7) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button6.Text = p
+        Button6.Enabled = False
+        nowPan(6) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button5.Text = p
+        Button5.Enabled = False
+        nowPan(5) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button4.Text = p
+        Button4.Enabled = False
+        nowPan(4) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button3.Text = p
+        Button3.Enabled = False
+        nowPan(3) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button2.Text = p
+        Button2.Enabled = False
+        nowPan(2) = StringToSteps(p)
+        Check()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim p As String
+        p = getnextStepinString()
+        Button1.Text = p
+        Button1.Enabled = False
+        nowPan(1) = StringToSteps(p)
+        Check()
+    End Sub
+    Private Function StringToSteps(t As String) As Steps
+        Select Case t
+            Case "X"
+                Return 1
+            Case "O"
+                Return 0
+        End Select
+    End Function
+End Class
